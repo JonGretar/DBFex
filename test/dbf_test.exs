67afed63e2ab26ff4c23b23d2807ba28f588e036
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2023-10-14 16:44:31 +0300

    Little bit of test massaging

--- test/dbf_test.exs
+++ test/dbf_test.exs
@@ -2,38 +2,64 @@ defmodule DBFTest do
   use ExUnit.Case
   doctest DBF
 
-  test "reads records" do
-    db = DBF.open!("test/dbf_files/bayarea_zipcodes.dbf")
-    DBF.read_records(db)
-    assert db.number_of_records == 187
-    DBF.close!(db)
+  describe "When reading the bay area zip codes file" do
+    setup do
+      db = DBF.open!("test/dbf_files/bayarea_zipcodes.dbf")
+      on_exit(fn -> DBF.close!(db) end)
+      {:ok, db: db}
+    end
+
+    test "it reads the version", context do
+      assert context.db.version == 3
+    end
+
+    test "it reads the last updated date", context do
+      assert context.db.last_updated == ~D[2009-06-16]
+    end
+
+    test "it reads the number of records", context do
+      assert context.db.number_of_records == 187
+    end
+
+    test "it gets the first record", context do
+      assert DBF.get(context.db, 0)
+    end
+
+    test "then the number of records should match the header", context do
+      assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
+    end
   end
 
-  describe "Reading version (03) dBase III without memo file" do
+  describe "When reading version (03) dBase III without memo file" do
     setup do
       db = DBF.open!("test/dbf_files/dbase_03.dbf")
       on_exit(fn -> DBF.close!(db) end)
       {:ok, db: db}
     end
 
-    test "reads the version", context do
+    test "it reads the version", context do
       assert context.db.version == 3
     end
 
-    test "reads the last updated date", context do
+    test "it reads the last updated date", context do
       assert context.db.last_updated == ~D[1905-07-13]
     end
 
-    test "reads the number of records", context do
+    test "it reads the number of records", context do
       assert context.db.number_of_records == 14
     end
 
-    test "Gets the first record", context do
+    test "it gets the first record", context do
       assert DBF.get(context.db, 0)
     end
+
+    test "then the number of records should match the header", context do
+      assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
+    end
+
   end
 
-  describe "Reading version (8b) dBase IV with memo file" do
+  describe "When reading version (8b) dBase IV with memo file" do
     setup do
       db = DBF.open!("test/dbf_files/dbase_8b.dbf")
       on_exit(fn -> DBF.close!(db) end)
@@ -55,30 +81,40 @@ defmodule DBFTest do
     test "Gets the first record", context do
       assert DBF.get(context.db, 0)
     end
+
+    test "then the number of records should match the header", context do
+      assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
+    end
+
   end
 
-  describe "Reading version (30) Visual FoxPro" do
+  describe "When reading version (30) Visual FoxPro" do
     setup do
       db = DBF.open!("test/dbf_files/dbase_30.dbf")
       on_exit(fn -> DBF.close!(db) end)
       {:ok, db: db}
     end
 
-    test "reads the version", context do
+    test "it reads the version", context do
       assert context.db.version == 48
     end
 
-    test "reads the last updated date", context do
+    test "it reads the last updated date", context do
       assert context.db.last_updated == ~D[1906-09-09]
     end
 
-    test "reads the number of records", context do
+    test "it reads the number of records", context do
       assert context.db.number_of_records == 34
     end
 
-    test "Gets the first record", context do
+    test "it gets the first record", context do
       assert DBF.get(context.db, 0)
     end
+
+    test "then the number of records should match the header", context do
+      assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
+    end
+
   end
 
 end
