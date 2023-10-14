b702df9b0fe9f4ab7c1793f553ca50f49ebad712
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2023-10-14 17:11:42 +0300

    Implementing the most basic of Enumerable function

--- lib/dbf/database.ex
+++ lib/dbf/database.ex
@@ -6,7 +6,8 @@ defmodule DBF.Database do
     :number_of_records,
     :header_bytes,
     :record_bytes,
-    :fields
+    :fields,
+    position: 0
   ]
   @type t :: %DBF.Database{
     device: File.stream,
@@ -15,8 +16,42 @@ defmodule DBF.Database do
     number_of_records: integer,
     header_bytes: integer,
     record_bytes: integer,
-    fields: [{binary, binary}]
+    fields: [{binary, binary}],
+    position: integer
   }
 
+end
+
+# Define the Enumerable implentation for the database.
+defimpl Enumerable, for: DBF.Database do
+  def count(db) do
+    # {:ok, db.number_of_records}
+    {:error, __MODULE__}
+  end
+
+  def reduce(db, {:cont, acc}, fun) do
+    if (db.position == db.number_of_records) do
+      {:done, acc}
+    else
+      record = DBF.get(db, db.position)
+    reduce(Map.put(db, :position, db.position + 1), fun.(record, acc), fun)
+    end
+  end
+
+  def reduce(_db, {:halt, acc}, _fun) do
+    {:halted, acc}
+  end
+
+  def reduce(db, {:suspend, acc}, fun) do
+    {:suspended, acc, &reduce(db, &1, fun)}
+  end
+
+  def slice(_array) do
+    {:error, __MODULE__}
+  end
+
+  def member?(_array, _element) do
+    {:error, __MODULE__}
+  end
 
 end
