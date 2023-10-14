fc453ba177efe8c3cb8fb1fa9bd1503848654992
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2023-10-14 11:26:55 +0300

    Updated with some real test

--- test/dbf_test.exs
+++ test/dbf_test.exs
@@ -9,4 +9,76 @@ defmodule DBFTest do
     DBF.close!(db)
   end
 
+  describe "Reading version (03) dBase III without memo file" do
+    setup do
+      db = DBF.open!("test/dbf_files/dbase_03.dbf")
+      on_exit(fn -> DBF.close!(db) end)
+      {:ok, db: db}
+    end
+
+    test "reads the version", context do
+      assert context.db.version == 3
+    end
+
+    test "reads the last updated date", context do
+      assert context.db.last_updated == ~D[1905-07-13]
+    end
+
+    test "reads the number of records", context do
+      assert context.db.number_of_records == 14
+    end
+
+    test "Gets the first record", context do
+      assert DBF.get(context.db, 0)
+    end
+  end
+
+  describe "Reading version (8b) dBase IV with memo file" do
+    setup do
+      db = DBF.open!("test/dbf_files/dbase_8b.dbf")
+      on_exit(fn -> DBF.close!(db) end)
+      {:ok, db: db}
+    end
+
+    test "reads the version", context do
+      assert context.db.version == 139
+    end
+
+    test "reads the last updated date", context do
+      assert context.db.last_updated == ~D[2000-06-12]
+    end
+
+    test "reads the number of records", context do
+      assert context.db.number_of_records == 10
+    end
+
+    test "Gets the first record", context do
+      assert DBF.get(context.db, 0)
+    end
+  end
+
+  describe "Reading version (30) Visual FoxPro" do
+    setup do
+      db = DBF.open!("test/dbf_files/dbase_30.dbf")
+      on_exit(fn -> DBF.close!(db) end)
+      {:ok, db: db}
+    end
+
+    test "reads the version", context do
+      assert context.db.version == 48
+    end
+
+    test "reads the last updated date", context do
+      assert context.db.last_updated == ~D[1906-09-09]
+    end
+
+    test "reads the number of records", context do
+      assert context.db.number_of_records == 34
+    end
+
+    test "Gets the first record", context do
+      assert DBF.get(context.db, 0)
+    end
+  end
+
 end
