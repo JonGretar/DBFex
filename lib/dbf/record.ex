2d7ac00821a370a45b0e95a4c43695bf9142046c
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2023-10-16 14:02:42 +0300

    Fix reading memos and floats

--- lib/dbf/record.ex
+++ lib/dbf/record.ex
@@ -7,19 +7,25 @@ defmodule DBF.Record do
     Enum.reverse(acc)
   end
   defp parse_record(db, [field| more_fields], data, acc) do
-    <<text::binary-size(field.length), rest::binary>> = data
-    value = transpose_field(db, field, text)
+    <<raw_value::binary-size(field.length), rest::binary>> = data
+    value = transpose_field(db, field, raw_value)
     parse_record(db, more_fields, rest, Map.put(acc, field.name, value))
   end
   defp parse_record(_db, _fields, _data, _acc) do
     throw "Invalid Record"
   end
 
+  defp transpose_field(_db, _field, "") do
+    nil
+  end
   defp transpose_field(_db, %{type: "C"}, value) do
     value |> String.trim()
   end
   defp transpose_field(_db, %{type: "F"}, value) do
-    value |> String.trim() |> String.to_float()
+    case value |> String.trim() do
+      "" -> nil
+      new_value -> new_value |> String.to_float()
+    end
   end
   defp transpose_field(_db, %{type: "I"}, value) do
     value |> String.trim() |> String.to_integer()
@@ -42,8 +48,16 @@ defmodule DBF.Record do
     end
   end
   defp transpose_field(db, %{type: "M"}, value) do
-    block = value |> String.trim() |> String.to_integer()
-    DBF.Memo.get_block(db.memo_file, block)
+    new_value = value |> String.trim()
+    if (new_value == "") do
+      nil
+    else
+      block = new_value |> String.to_integer()
+      DBF.Memo.get_block(db.memo_file, block)
+    end
+  end
+  defp transpose_field(_db, %{type: "D"}, "        ") do
+    nil
   end
   defp transpose_field(_db, %{type: "D"}, <<year::binary-size(4), month::binary-size(2), day::binary-size(2)>>) do
     Date.from_iso8601!("#{year}-#{month}-#{day}")
