5a6f59aad21c1732807a2a2820f9cb82ead11dda
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2023-10-17 10:11:49 +0300

    Rename silly function name

--- lib/dbf/record.ex
+++ lib/dbf/record.ex
@@ -8,29 +8,29 @@ defmodule DBF.Record do
   end
   defp parse_record(db, [field| more_fields], data, acc) do
     <<raw_value::binary-size(field.length), rest::binary>> = data
-    value = transpose_field(db, field, raw_value)
+    value = read_field(db, field, raw_value)
     parse_record(db, more_fields, rest, Map.put(acc, field.name, value))
   end
   defp parse_record(_db, _fields, _data, _acc) do
     throw "Invalid Record"
   end
 
-  defp transpose_field(_db, _field, "") do
+  defp read_field(_db, _field, "") do
     nil
   end
-  defp transpose_field(_db, %{type: "C"}, value) do
+  defp read_field(_db, %{type: "C"}, value) do
     value |> String.trim()
   end
-  defp transpose_field(_db, %{type: "F"}, value) do
+  defp read_field(_db, %{type: "F"}, value) do
     case value |> String.trim() do
       "" -> nil
       new_value -> new_value |> String.to_float()
     end
   end
-  defp transpose_field(_db, %{type: "I"}, value) do
+  defp read_field(_db, %{type: "I"}, value) do
     value |> String.trim() |> String.to_integer()
   end
-  defp transpose_field(_db, %{type: "L"}, value) do
+  defp read_field(_db, %{type: "L"}, value) do
     # Read logical operations.
     # if value is any of YyTt it is TRUE. if it is any of NnFf it is FALSE. Else it's nil
     case value do
@@ -40,14 +40,14 @@ defmodule DBF.Record do
       other -> raise "Illegal logical value: #{other}"
     end
   end
-  defp transpose_field(_db, %{type: "N"}, value) do
+  defp read_field(_db, %{type: "N"}, value) do
     # TODO: Should we be always returning a float? Or should we be returning an integer if there is no decimal?
     case value |> String.trim() |> Float.parse() do
       {number, _} -> number
       :error -> nil
     end
   end
-  defp transpose_field(db, %{type: "M"}, value) do
+  defp read_field(db, %{type: "M"}, value) do
     new_value = value |> String.trim()
     if (new_value == "") do
       nil
@@ -56,16 +56,16 @@ defmodule DBF.Record do
       DBF.Memo.get_block(db.memo_file, block)
     end
   end
-  defp transpose_field(_db, %{type: "D"}, "        ") do
+  defp read_field(_db, %{type: "D"}, "        ") do
     nil
   end
-  defp transpose_field(_db, %{type: "D"}, <<year::binary-size(4), month::binary-size(2), day::binary-size(2)>>) do
+  defp read_field(_db, %{type: "D"}, <<year::binary-size(4), month::binary-size(2), day::binary-size(2)>>) do
     Date.from_iso8601!("#{year}-#{month}-#{day}")
   end
-  defp transpose_field(_db, %{type: "D"}, _) do
+  defp read_field(_db, %{type: "D"}, _) do
     throw "Invalid Date"
   end
-  defp transpose_field(_db, field, _value) do
+  defp read_field(_db, field, _value) do
     throw "Unhandled Field Type: #{inspect(field)}"
   end
 
