afb2b957f096251ee00e7cb895c84e0a266d24ad
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2023-10-19 08:33:40 +0300

    FoxPro tests

--- test/dbf_test.exs
+++ test/dbf_test.exs
@@ -88,6 +88,34 @@ defmodule DBFTest do
 
   end
 
+  describe "When reading version (32) Visual FoxPro with field type Varchar or Varbinary" do
+    setup do
+      db = DBF.open!("test/dbf_files/dbase_32.dbf")
+      on_exit(fn -> DBF.close!(db) end)
+      {:ok, db: db}
+    end
+
+    test "it reads the version", context do
+      assert context.db.version == 0x32
+    end
+
+    test "it reads the last updated date", context do
+      assert context.db.last_updated == ~D[1912-01-29]
+    end
+
+    test "it reads the number of records", context do
+      assert context.db.number_of_records == 1
+    end
+
+    test "it gets the first record", context do
+      assert DBF.get(context.db, 0)
+    end
+
+    test "then the number of records should match the header", context do
+      assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
+    end
+  end
+
   describe "When reading version (8b) dBase IV with memo file" do
     setup do
       db = DBF.open!("test/dbf_files/dbase_8b.dbf")
@@ -143,7 +171,34 @@ defmodule DBFTest do
     test "then the number of records should match the header", context do
       assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
     end
+  end
 
+  describe "When reading version (f5) FoxPro with memo file" do
+    setup do
+      db = DBF.open!("test/dbf_files/dbase_f5.dbf")
+      on_exit(fn -> DBF.close!(db) end)
+      {:ok, db: db}
+    end
+
+    test "it reads the version", context do
+      assert context.db.version == 0xf5
+    end
+
+    test "it reads the last updated date", context do
+      assert context.db.last_updated == ~D[1904-02-28]
+    end
+
+    test "it reads the number of records", context do
+      assert context.db.number_of_records == 975
+    end
+
+    test "it gets the first record", context do
+      assert DBF.get(context.db, 0)
+    end
+
+    test "then the number of records should match the header", context do
+      assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
+    end
   end
 
 end
