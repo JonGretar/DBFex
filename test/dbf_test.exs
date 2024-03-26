755a1f5f645da77b9724505d9a01eb4f30a65661
Jon Gretar Borgthorsson <jongretar@jongretar.com>
2024-03-26 17:05:38 +0200

    Testing of error

--- test/dbf_test.exs
+++ test/dbf_test.exs
@@ -119,6 +119,27 @@ defmodule DBFTest do
       assert DBF.get(context.db, 0)
     end
 
+    test "the first record is all that we wanted", context do
+      record = %{
+        "ACTIVE" => true,
+        "AGRPCOUNT" => 0.0,
+        "CATCOUNT" => 2.0,
+        "CODE" => "1",
+        "COST" => 0.0,
+        "DESC" => "Our Original assortment...a little taste of heaven for everyone.  Let us\r\nselect a special assortment of our chocolate and pastel favorites for you.\r\nEach petit four is its own special hand decorated creation. Multi-layers of\r\nmoist cake with combinations of specialty fillings create memorable cake\r\nconfections. Varietes include; Luscious Lemon, Strawberry Hearts, White\r\nChocolate, Mocha Bean, Roasted Almond, Triple Chocolate, Chocolate Hazelnut,\r\nGrand Orange, Plum Squares, Milk chocolate squares, and Rasp",
+        "ID" => 87.0,
+        "IMAGE" => "graphics/00000001/1.jpg",
+        "NAME" => "Assorted Petits Fours",
+        "ORDER" => 87.0,
+        "PGRPCOUNT" => 0.0,
+        "PRICE" => 0.0,
+        "TAXABLE" => true,
+        "THUMBNAIL" => "graphics/00000001/t_1.jpg",
+        "WEIGHT" => 5.51
+      }
+      assert {:record, record} == DBF.get(context.db, 0)
+    end
+
     test "then the number of records should match the header", context do
       assert context.db.number_of_records == context.db |> Enum.to_list() |> length()
     end
