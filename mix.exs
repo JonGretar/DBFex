903096e4038e4325fef9f785b0b81dbd4145c32e
Jón Grétar Borgþórsson <jon@montel.fi>
2026-09-03 14:52:07 +0300

    Release v0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DBF.MixProject do
   def project do
     [
       app: :dbf_ex,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       package: package(),
